409ce30a3c1622cc094e885563e193f7428f90c3
Graham Watts <34165628+GingerGraham@users.noreply.github.com>
2024-12-10 08:22:26 +0000

    feat(unit-files): adding unit file and journal log example

---
+++ UnitFileTest/log_file_and_journal.sh
@@ -0,0 +1,4 @@
+# File: /usr/local/bin/log_time.sh
+LOG_FILE="/var/log/time_log.log"
+LOG_MESSAGE="$(date '+%Y-%m-%d %H:%M:%S') - Script executed"
+echo $LOG_MESSAGE | tee -a $LOG_FILE | logger -t log_time_script
---
+++ UnitFileTest/watch_logs.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# Watch the logs for the log_time_script journalctl tag
+journalctl -f -t log_time_script
